02ea6d14cff6bcf0b2f47a7cadf6625acb06602a
EmanueleRangel <emanuele.rangel52@gmail.com>
2025-01-15 17:20:38 -0300

    add first file

---
+++ app/otimizacao_voos.py
@@ -0,0 +1,15 @@
+import time
+import random
+import math
+
+pessoas = [('Amanda', 'CWB'), 
+           ('Bruno', 'GIG'), 
+           ('Carlos', 'POA'), 
+           ('Diana', 'FLN'), 
+           ('Elaine','CNF'),
+           ('Hugo', 'GYN')]
+
+destino = 'GRU'
+
+voos = {}
+
